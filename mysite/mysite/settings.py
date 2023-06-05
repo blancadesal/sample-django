b26f7949b908221fce244626fb93c03411d93b06
Slavina Stefanova <sstefanova@wikimedia.org>
2023-06-05 10:48:43 +0200

    read secret from file

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -21,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+with open("/data/project/django-test/secret_key.txt") as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
