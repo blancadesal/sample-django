783834b9c8a70846ca24564e1ded0060a1370960
Slavina Stefanova <sstefanova@wikimedia.org>
2023-06-06 10:38:55 +0200

    db settings for prod

--- mysite/settings.py
+++ mysite/settings.py
@@ -84,13 +84,20 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+if IN_PROD:
+	DATABASES = {
+		'default': {
+			'ENGINE': 'django.db.backends.sqlite3',
+			'NAME': 'sqlite:///data/project/django-test/db.sqlite3',
+		}
+	}
+else:
+	DATABASES = {
+		'default': {
+			'ENGINE': 'django.db.backends.sqlite3',
+			'NAME': BASE_DIR / 'db.sqlite3',
+		}
+	}
 
 
 # Password validation
