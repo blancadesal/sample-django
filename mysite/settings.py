b4d83558de88e3fe41c221e65745db0b3307d633
Slavina Stefanova <sstefanova@wikimedia.org>
2023-06-07 14:19:01 +0200

    revert to serving static files from pod

--- mysite/settings.py
+++ mysite/settings.py
@@ -133,10 +133,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-DATA_DIR = Path('/data/project/django-test') if IN_PROD else BASE_DIR
+# DATA_DIR = Path('/data/project/django-test') if IN_PROD else BASE_DIR
 
 STATIC_URL = '/static/'
-STATIC_ROOT = DATA_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'static'
 
 
 # Default primary key field type
