d34a17624e21b8c6c4ead15ad4bdedbb05354113
Slavina Stefanova <sstefanova@wikimedia.org>
2023-07-05 10:52:33 +0200

    mucking around with settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -21,9 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
+SECRET_KEY = None
 
-IN_PROD = int(os.environ["PROD"])
+IN_PROD = 0
 
 # SECURITY WARNING: don't run with debug turned on in production!
 if not IN_PROD:
